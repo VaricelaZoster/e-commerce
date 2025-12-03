077662a49a4002e00a2319a47deeef68faa91b47
VaricelaZoster <162027550+VaricelaZoster@users.noreply.github.com>
2025-12-03 18:16:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navigation from './customer/components/Navigation/Navigation'
+
+const App = () => {
+  return (
+    <div>
+      <Navigation/>
+      <div></div>
+    </div>
+  )
+}
+
+export default App
